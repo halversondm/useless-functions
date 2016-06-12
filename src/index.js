3bad61357de724d4c58a89e9b5e5d9ed94d5429a
Dan Halverson <daniel.m.halverson@gmail.com>
2016-06-12 08:58:06 -0500

    initial functions

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Daniel on 6/12/2016.
+ */
+"use strict";
+
+export function name() {
+  return "Dan Halverson";
+}
+
+export function address() {
+  return "123 Main Street, Anytown, USA";
+}
