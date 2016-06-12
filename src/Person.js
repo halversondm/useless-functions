1979e8650b7bab7dd362fa3a5ad771c692e1fd87
Dan Halverson <daniel.m.halverson@gmail.com>
2016-06-12 16:37:41 -0500

    adding a class

--- src/Person.js
+++ src/Person.js
@@ -1,14 +1,13 @@
 /**
  * Created by Daniel on 6/12/2016.
  */
-"use strict";
 
 import {name} from "./name";
 import {address} from "./address";
-import {Person} from "./Person";
 
-export default {
-  name,
-  address,
-  Person
+export class Person {
+
+  details() {
+    return name() + " " + address();
+  }
 }
